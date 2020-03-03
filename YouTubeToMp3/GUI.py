cdf7ce76e2c6138205e853bda90217b4eaffbdc2
Zac Cripps <zachary.cripps@griffithuni.edu.au>
2020-03-03 17:15:04 +1000

    edited sizing and created textfield

--- YouTubeToMp3/GUI.py
+++ YouTubeToMp3/GUI.py
@@ -1,5 +1,32 @@
-import wx # First things, first. Import the wxPython package.
-app = wx.App() # Next, create an application object. 
-frm = wx.Frame(None, wx.ID_ANY, title="Youtube to MP3") # Then a frame. 
-frm.Show() # Show it. 
-app.MainLoop() # Start the event loop. 
+import wx
+  
+class Mywin(wx.Frame): 
+   def __init__(self, parent, title): 
+      super(Mywin, self).__init__(parent, title = title,size = (350,250))
+		
+      panel = wx.Panel(self) 
+      vbox = wx.BoxSizer(wx.VERTICAL) 
+
+		
+      hbox3 = wx.BoxSizer(wx.HORIZONTAL) 
+      l3 = wx.StaticText(panel, -1, "Enter URL") 
+		
+      hbox3.Add(l3,1, wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+      self.t3 = wx.TextCtrl(panel,size = (200,100),style = wx.TE_MULTILINE) 
+		
+      hbox3.Add(self.t3,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+      vbox.Add(hbox3)  
+
+      panel.SetSizer(vbox) 
+        
+      self.Centre() 
+      self.Show() 
+      self.Fit()  
+		
+   def OnKeyTyped(self, event): 
+       print(event.GetString())
+
+		
+app = wx.App() 
+Mywin(None,  'Youtube To Mp3')
+app.MainLoop()
